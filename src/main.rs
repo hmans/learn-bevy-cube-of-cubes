7d9056479fdb7714ba67ea5b1156c9243513563b
Hendrik Mans <hendrik@mans.de>
2023-10-17 11:13:56 +0200

    Add a material

--- src/main.rs
+++ src/main.rs
@@ -14,19 +14,27 @@ fn setup_camera(mut commands: Commands) {
     });
 }
 
-fn setup_cubes(mut commands: Commands, mut mesh_assets: ResMut<Assets<Mesh>>) {
+fn setup_cubes(
+    mut commands: Commands,
+    mut mesh_assets: ResMut<Assets<Mesh>>,
+    mut material_assets: ResMut<Assets<StandardMaterial>>,
+) {
     let mesh = mesh_assets.add(shape::Box::new(1., 1., 1.).into());
+    let material = material_assets.add(Color::rgb(0.5, 0.5, 1.0).into());
 
     for x in -5..5 {
         for y in -5..5 {
             for z in -5..5 {
                 commands.spawn(PbrBundle {
                     mesh: mesh.clone(),
+                    material: material.clone(),
+
                     transform: Transform::from_xyz(
                         x as f32 * 1.75,
                         y as f32 * 1.75,
                         z as f32 * 1.75,
                     ),
+
                     ..default()
                 });
             }
