96fea4d834ac78c98fa650701add2236bf005659
Hendrik Mans <hendrik@mans.de>
2023-10-17 10:39:23 +0200

    Get a simple Bevy app going

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!!!!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, hello_world)
+        .run();
+}
+
+fn hello_world() {
+    println!("Hello World!");
 }
